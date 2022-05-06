8a48bdc3a83673220df3c4a0d28000708b546e7a
Cesar  Acual <augustoacual@gmail.com>
2022-05-05 19:20:22 -0600

    Adjust Navbar

--- src/components/ExchangeNavbar.vue
+++ src/components/ExchangeNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <nav class="navbar">
+    <nav class="navbar" :class="$route.path === '/' ? '' : 'with-background'">
       <div class="container">
         <div class="navbar-brand">
           <a
