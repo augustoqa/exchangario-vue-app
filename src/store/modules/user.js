79f76e5edc04fbb46ce72ba96ca826820f303f27
Cesar Quisquinay <augustoacual@gmail.com>
2022-11-13 21:22:13 -0600

    Create user Profile

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,6 @@
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import db from '../../db'
 
 export default {
   namespaced: true,
@@ -19,23 +21,33 @@ export default {
     },
   },
   actions: {
-    async register({ commit, dispatch }, { email, password }) {
+    async register({ commit, dispatch }, { email, password, username }) {
       commit('setRegisterIsProcessing', true)
       commit('setRegisterError', '')
 
       try {
-        const userCredentials = await createUserWithEmailAndPassword(
+        const { user } = await createUserWithEmailAndPassword(
           getAuth(),
           email,
           password
         )
-        return userCredentials.user
+
+        await dispatch('createUserProfile', {
+          id: user.uid,
+          username,
+          avatar: 'https://cdn-icons-png.flaticon.com/512/4086/4086679.png',
+          credit: 0,
+          exchanges: [],
+        })
       } catch (e) {
         commit('setRegisterError', e.message)
         dispatch('toast/error', e.message, { root: true })
       } finally {
         commit('setRegisterIsProcessing', false)
       }
+    },
+    async createUserProfile(_, { id, ...profile }) {
+      await setDoc(doc(db, 'users', id), profile)
     },
   },
 }
