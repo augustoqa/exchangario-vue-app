f7c86382f9c9bfe9111feac9951ee0cb08963df5
Augusto Acual <augustoacual@gmail.com>
2023-10-28 10:06:58 -0600

    Check auth profile page

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,7 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     component: ProfilePage,
+    meta: { onlyAuthUser: true },
   },
   {
     path: '/register',
@@ -48,10 +49,16 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach(async (to, _, next) => {
-  const isAuth = await getAuth().currentUser
+router.beforeEach((to, _, next) => {
+  const isAuth = !!getAuth().currentUser
 
-  if (to.meta.onlyGuestUser) {
+  if (to.meta.onlyAuthUser) {
+    if (isAuth) {
+      next()
+    } else {
+      next({ name: 'Login' })
+    }
+  } else if (to.meta.onlyGuestUser) {
     if (isAuth) {
       next({ name: 'Home' })
     } else {
